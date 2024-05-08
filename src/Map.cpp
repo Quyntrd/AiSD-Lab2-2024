4b88259efe4c756a8141b73d8732f6fcc631198e
Quyntrd <daniil.terebilkib@gmail.com@git config --global user.name Quyntrdgit config --global user.email daniil.terebilkib@gmail.com@>
2024-05-08 19:15:16 +0400

    added constructors, operator=, methods print, insert, insert_or_assign

---
+++ src/Map.cpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <random>
+
+using namespace std;
+
+template<typename Key, typename Value>
+class UnorderedMap {
+	struct Pair {
+		Key key;
+		Value value;
+		Pair() : key(), value() {};
+		Pair(const Key& ke, const Value& val) : key(ke), value(val) {};
+	};
+	vector<Pair> _data;
+	size_t _size;
+	size_t hash(const Key& key) const {
+		const int w = sizeof(size_t) * 8;
+		size_t a = 2654435761;
+		return (key * a % (1 << w) >> (w / 2);
+	}
+};
